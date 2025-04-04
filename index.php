b411d8d2362cca1b3fb15f0b6cbaaa097c11ae28
Aragorn-dGA <157470770+Aragorn-dGA@users.noreply.github.com>
2025-04-04 11:43:57 +0200

    Update index.php
    enlève des codes sql

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
 
 require('Controleur/Controleur.php');
-
-try {
-    if (isset($_GET['action'])) {
-      if ($_GET['action'] == 'projet') {
-        if (isset($_GET['id'])) {
-          $idProjet = intval($_GET['id']);
-          if ($idProjet != 0)
-            projet($idProjet);
-          else
-            throw new Exception("Identifiant de projet non valide");
-        }
-        else
-          throw new Exception("Identifiant de projet non défini");
-      }
-      else
-        throw new Exception("Action non valide");
-    }
-    else {
-      accueil();  // action par défaut
-    }
-  }
-  catch (Exception $e) {
-      erreur($e->getMessage());
-  }
+?>
